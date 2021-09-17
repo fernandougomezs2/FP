7b52de259db687220c1e3164a4738ee8c2aa93c0
fernandougomezs2 <fernandougomezs2@gmail.com>
2021-09-17 14:36:39 -0500

    Examen Unidad 1

---
+++ Examen.py
@@ -0,0 +1,32 @@
+print("Datos del alumno:")
+
+print("Nombre: Fernando Ulises Gomwz Sanchez")
+
+print("Lugar de nacimiento: Rincon de Romos")
+
+print("Fecha de nacimiento: 08-12-03")
+
+print("Color favorito: Rojo")
+
+print("Animal favorito: El leon")
+
+print("Cuestionario:")
+
+print("¿Que es la programacion?")
+print("Es la acción de desarrollar que involucra ordenar, estructurar o componer una secuencia de ocupaciones cronológicas para llevar a cabo un objetivo.")
+
+print("¿Por qué es importante la programación?")
+print("es primordial para acrecentar el progreso tecnológico en las industrias que para realizar sus funcionalidades requieren de sitios y aplicaciones creadas desde los códigos ")
+
+print("¿Que es herencia?")
+print("En programación dirigida a objetos, la herencia es, luego de la suma o estructura, el mecanismo más usado para conseguir ciertos de las metas más preciados en el desarrollo de programa ")
+
+print("¿Que es poliformismo?")
+print("el polimorfismo tiene relación con la propiedad por la que es viable mandar mensajes sintácticamente equivalentes a objetos de tipos diversos. ")
+
+print("¿Que es abstracción?")
+print("Es algo que está en el mundo de las ideas, los pensamientos, empero que no se puede concretar en algo material, que se logre tocar. Es aquella sobre la que no podemos instanciar objetos")
+
+print("¿Que es encapsulamiento")
+print("Ocultamiento del estado, o sea, de los datos integrante de un objeto de forma que unicamente se logre modificar por medio de las operaciones definidas para aquel objeto ")
+
