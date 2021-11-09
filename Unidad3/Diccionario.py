ccdd07577074d1996ccf2a03c3f76e670bdcf378
fernandougomezs2 <fernandougomezs2@gmail.com>
2021-11-08 20:20:32 -0600

    unidad tres diccionario

---
+++ Unidad3/Diccionario.py
@@ -0,0 +1,48 @@
+Usuario = {
+	"Nombre": "Fernando",
+	"Apellido": "Gomez",
+	"Correo": "fernandougomezs2@gamil.com",
+}
+
+print(Usuario)
+
+Tienda = {
+	
+	"Nombre": "Tienda de abarrotes",
+	"Productos1": "Comida",
+	"Productos2": "Refrescos",
+}
+
+print(Tienda)
+
+Coche = {
+	
+	"Marca": "Toyota",
+	"Color": "Rojo",
+	"Modelo": "2",
+	"Propietario": "Ulises",
+}
+
+print(Coche)
+
+Caballo = {
+	
+	"Nombre": "Nando",
+	"Edad": "7",
+	"Peso": "50kg",
+	"Tamaño": "1.40",
+
+}
+
+print(Caballo)
+
+Perro = {
+	
+	"Raza": "Pastor Aleman",
+	"Color": "Negro",
+	"Años": "5",
+	"Tamaño": "1.20",
+}
+
+print(Perro)
+
